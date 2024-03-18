9e1e5e3545cc06a6d87f9eec5eb10c11ebb480df
James Chase <gochases@gmail.com>
2024-03-18 16:11:38 -0600

    Add setup function and restructure menu

--- Program.cs
+++ Program.cs
@@ -8,10 +8,32 @@ namespace inventory_manager
 {
     internal class Program
     {
-        static void menu()
+        // Runs basic setup for the program, checks if storage file exists
+        // Parameters: None
+        // Returns: void
+        private static void Setup()
         {
-            bool valid_inp = false;
-            int menu_val;
+            string data_file = "inventory.txt";
+
+            if (!File.Exists(data_file))
+            {
+                try
+                {
+                    File.Create(data_file);
+                }
+                catch
+                {
+                    Console.WriteLine();
+                    throw new Exception("Something went wrong. Aborting program...");
+                }
+            }
+        }
+
+        // Displays menu and handles user selected option
+        // Parameters: None
+        // Returns: int (contains user choice)
+        private static int Menu()
+        {
             string userInp;
 
             Console.WriteLine("##################################################");
@@ -21,35 +43,20 @@ namespace inventory_manager
             Console.WriteLine("##### [2] Add a new part                     #####");
             Console.WriteLine("##### [3] Format Inventory Data Sheet        #####");
             Console.WriteLine("##### [4] Lookup part via part number        #####");
+            Console.WriteLine("##### [5] End Program                        #####");
             Console.WriteLine("#####----------------------------------------#####");
             Console.WriteLine("##################################################");
 
-            while (!valid_inp)
+            while (true)
             {
                 Console.Write("> ");
                 userInp = Console.ReadLine();
 
-                if (int.TryParse(userInp, out menu_val))
+                if (int.TryParse(userInp, out int menu_val))
                 {
-                    valid_inp = true;
-                    switch (menu_val)
-                    {
-                        case 1:
-                            Console.WriteLine(menu_val);
-                            break;
-                        case 2:
-                            Console.WriteLine(menu_val);
-                            break;
-                        case 3:
-                            Console.WriteLine(menu_val);
-                            break;
-                        case 4:
-                            Console.WriteLine(menu_val);
-                            break;
-                        default:
-                            Console.WriteLine(userInp + " is not a valid menu option.\n");
-                            valid_inp = false;
-                            break;
+                    if (menu_val > 0 && menu_val < 6) return menu_val;
+                    else {
+                        Console.WriteLine(userInp + " is not a valid menu option.\n");
                     }
                 }
                 else
@@ -59,16 +66,45 @@ namespace inventory_manager
             }
         }
 
+        // Main Function
         static void Main()
         {
+            try
+            {
+                Setup();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+                return;
+            }
+
+            int selection = 0;
+
             Console.WriteLine("##################################################");
             Console.WriteLine("##### Welcome to Inventory Management System #####");
             Console.WriteLine("##################################################");
             Console.WriteLine("Press Enter to continue...");
             Console.ReadLine();
+
             Console.Clear();
 
-            menu();
+            while (selection != 5)
+            {
+                selection = Menu();
+                switch(selection)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
         }
     }
 }
