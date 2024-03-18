1b5d38202bc7bb6298324084d785198031b7a4ab
James Chase <gochases@gmail.com>
2024-03-18 14:46:47 -0600

    Fix version control tracking

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace inventory_manager
+﻿/*
+ * Name: James A. Chase
+ * File: Program.cs
+ * Date: 18 March 2024
+*/
+
+namespace inventory_manager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("##################################################");
+            Console.WriteLine("##### Welcome to Inventory Management System #####");
+            Console.WriteLine("##################################################");
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+            
         }
     }
 }
